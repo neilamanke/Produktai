67d60e629a29618bc1ac697145d7659921f06194
neilamanke <neilamanke@gmail.com>
2025-02-07 19:07:11 +0200

    Delete components directory

--- components/CategorySelect.jsx
+++
@@ -1,9 +0,0 @@
-import './UpdateStyle.css'
-
-export function CategorySelect(props) {
-  return (
-    <>
-      <option className='update-select' value={`${props.id}`}>{props.categoryType}</option>
-    </>
-  );
-}
