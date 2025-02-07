e2a41e8821d109e8fab128f074a3c328b6bac5de
neilamanke <neilamanke@gmail.com>
2025-02-07 19:08:57 +0200

    Delete middleware directory

--- middleware/errorHandlerMiddleware.mjs
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err, req, res) => {
-    res.status(500).json({
-      status: "error",
-      message: `Internal Server Error, ${err.message}`,
-    });
-  };
-  
-  export default errorHandler;
